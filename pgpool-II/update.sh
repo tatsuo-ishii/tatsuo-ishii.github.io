f7637a6e493bc11315548523e14906b5f3503030
Tatsuo Ishii <ishii@sraoss.co.jp>
2018-08-08 11:28:39 +0900

    Update PostgreSQL news and Pgpool-II 4.0 dev documents.

---
+++ pgpool-II/update.sh
@@ -0,0 +1,5 @@
+dir=/home/t-ishii/work/
+cd $dir/tatsuo-ishii.github.io/pgpool-II/current
+cp $dir/Pgpool-II/current/pgpool2/doc/src/sgml/html/* .
+cd $dir/tatsuo-ishii.github.io/pgpool-II/current-ja
+cp $dir/Pgpool-II/current/pgpool2/doc.ja/src/sgml/html/* .
